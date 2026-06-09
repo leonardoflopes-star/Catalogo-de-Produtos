4b5ad947ee3d77a871d24e762da2fd6fbae8089e
leonardoflopes-star <leonardo.f.lopes@uni9.edu.br>
2026-06-09 22:01:57 +0000

    Primeira versão

---
+++ CatalogoDeProdutos/Controllers/ProdutoController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Produto.Controllers 
+{
+    public class ProdutoController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            
+            List<string> nomesProdutos = new List<string> { "Teclado Mecânico", "Mouse Gamer", "Monitor 24'", "Fone de Ouvido", "Mousepad XL" };
+            
+            
+            List<double> precosProdutos = new List<double> { 250.00, 85.50, 899.90, 150.00, 45.00 };
+
+            
+            ViewBag.ListaNomes = nomesProdutos;
+            ViewBag.ListaPrecos = precosProdutos;
+
+            return View();
+        }
+    }
+}
---
+++ CatalogoDeProdutos/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CatalogoDeProdutos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
